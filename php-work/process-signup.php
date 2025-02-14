0df0dfeab51a36cd249cc64215cf8e3def62dbff
Pluto <ryan.morrell@gmail.com>
2025-02-14 11:26:33 -0800

    signup accesses DB and redirects on complete

--- php-work/process-signup.php
+++ php-work/process-signup.php
@@ -29,5 +29,23 @@ if($_POST["password"] !== $_POST["password_confirm"]){
 
 $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
+$mysqli = require __DIR__ . "/database.php";
 
-var_dump($password_hash);
+
+$sql = "INSERT INTO user (name, email, password_hash) VALUES (?,?,?)";
+
+$stmt = $mysqli->stmt_init();
+
+if( ! $stmt->prepare($sql)){
+    die("SQL error " . $mysqli->error);
+}
+
+$stmt->bind_param("sss",$_POST["name"], $_POST["email"], $password_hash);  
+
+
+if ($stmt->execute()){
+    header("Location : signup-success.html");
+    exit;
+}else{
+    die($mysqli->error . " " . $mysqli->errno);
+}
