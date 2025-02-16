3de66c19a444d9a0cde62bfb1c78373fb9a3f796
Pluto <ryan.morrell@gmail.com>
2025-02-16 15:20:55 -0800

    qr code generator shell working

---
+++ php-work/generateQR.php
@@ -0,0 +1,18 @@
+<?php
+
+require "vendor/autoload.php";
+
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PngWriter;
+
+
+$text = $_POST["text"];
+
+$qrCode = new QrCode($text);
+$writer = new PngWriter();
+
+$result = $writer->write($qrCode);
+
+header("Content-Type: " . $result->getMimeType());
+echo $result->getString();
+exit;
