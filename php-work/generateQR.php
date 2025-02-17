ec0b42986b8e99cb8210dec0b601eea9a622072a
Pluto <ryan.morrell@gmail.com>
2025-02-16 16:22:12 -0800

    Event Create form Preped for Database

--- php-work/generateQR.php
+++ php-work/generateQR.php
@@ -6,13 +6,21 @@ use Endroid\QrCode\QrCode;
 use Endroid\QrCode\Writer\PngWriter;
 
 
-$text = $_POST["text"];
+// before making the QR code create the Event instance in the DB
+// use the Event ID to create the QR Code 
+// QR code should go to a signup.php file where a global variable is set to the ID 
+// So: when the form submits it creates an attendee with the event ID set to the prepopulated field
+// link to signup.php/eventID in the QR Code
 
-$qrCode = new QrCode($text);
+// insert event into DB with $_POST details
+// get event PK
+// link to prepopulated signup with event ID
+
+$qrCode = new QrCode($eventID);
 $writer = new PngWriter();
 
 $result = $writer->write($qrCode);
 
 header("Content-Type: " . $result->getMimeType());
 echo $result->getString();
-exit;
+exit;   
