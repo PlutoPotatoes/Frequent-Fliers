2a408e562cf4b8b3dde835705bbc4464c3f4db51
Pluto <ryan.morrell@gmail.com>
2025-03-18 12:09:16 -0700

    reformatting

--- php-work/database.php
+++ php-work/database.php
@@ -1,15 +1,18 @@
 <?php
 
 // change these values to the correct database data
-$host = "database-1.c18souwm2mtl.us-east-2.rds.amazonaws.com";
-$dbname = "frequent_fliers";
-$username = "";
-$password = "";
+// Put these in a function we can include whereever needed??
+// return $mysqli/$conn
 
-$mysqli = new mysqli(hostname:$host,
-                    username: $username,
-                    password: $password,
-                    database: $dbname);
+
+
+//info is correct for Cianci's database but access is denied for my user
+$host = 'sql.cianci.io';
+$dbname = 'frequentfliers';
+$username = 'rmorrell';
+$password = 'e2VaSdfES6sU';
+
+$mysqli = new mysqli($host, $username, $password, $dbname);
 
 if ($mysqli->connect_errno){
     die("Connection error: " . $mysqli->connect_errno);
